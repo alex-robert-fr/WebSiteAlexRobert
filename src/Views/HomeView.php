719da8358e07c8b1d72966f09620931beab792e5
Alex Robert <contact.alexrobert04@gmail.com>
2021-05-31 12:16:36 +0200

    Add google analytics in home page

--- src/Views/HomeView.php
+++ src/Views/HomeView.php
@@ -9,6 +9,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= $router->fileUrl('/Src/Views/css/alex-mobile.css') ?>">
     <link rel="stylesheet" href="<?= $router->fileUrl('/Src/Views/css/alex-ordinateur.css') ?>">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7MM7J17T1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-C7MM7J17T1');
+    </script>
 </head>
 
 <body>
